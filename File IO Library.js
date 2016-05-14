add49d6c61b7ecfe40a0c1ce63af7ceb5385e1cb
Dark Tornado <dt3141592@naver.com>
2016-05-14 14:15:55 +0900

    Add files via upload

---
+++ File IO Library.js
@@ -0,0 +1,155 @@
+﻿File IO Library
+version : 1.0
+Maker : Dark Tornado
+© 2016 Dark Tornado, All rights reserved.
+--------------------
+
+
+const File = {
+makeFolder : function(path){
+try{
+var folder = new java.io.File(path);
+folder.mkdir();
+}
+catch(e){
+Dark.showDialog("Error from DCMS Library", e);
+}
+},
+create : function(path){
+try{
+var file = new java.io.File(path);
+file.createNewFile();
+}
+catch(e){
+Dark.showDialog("Error from DCMS Library", e);
+}
+},
+write : function(name, value){
+try{
+var file = new java.io.File(path);
+var fos = new java.io.FileOutputStream(file);
+var str = new java.lang.String(value);
+fos.write(str.getBytes());
+fos.close();
+}
+catch(e){
+Dark.showDialog("Error from DCMS Library", e)
+}
+},
+read : function(path){
+try{
+var file = new java.io.File(path);
+if(!file.exists()) return "";
+var fis = new java.io.FileInputStream(file);
+var isr = new java.io.InputStreamReader(fis);
+var br = new java.io.BufferedReader(isr);
+var str = br.readLine();
+var line = "";
+while((line=br.readLine())!=null){
+str += "\n"+line;
+}
+fis.close();
+isr.close();
+br.close();
+return str;
+}
+catch(e){
+Dark.showDialog("Error from DCMS Library", e)
+}
+},
+remove : function(path){
+try{
+var file = new java.io.File(path);
+if(file.exists()) file.delete();
+}
+catch(e){
+Dark.showDialog("Error from DCMS Library", e)
+}
+},
+move : function(path1, path2){
+try{
+var file1 = new java.io.File(path1);
+var file2 = new java.io.File(path2);
+file1.renameTo(file2);
+}
+catch(e){
+Dark.showDialog("Error from DCMS Library", e);
+}
+},
+getList : function(path){
+try{
+var file = new java.io.File(path);
+var dir = file.list().sort();
+var dir2 = [], dir3 = [];
+for(var n=0;n<dir.length;n++){
+var ff = new java.io.File(path+"/"+dir[n]);
+if(ff.isDirectory()) dir2.push(dir[n]);
+else dir3.push(dir[n]);
+}
+for(var n=0;n<dir3.length;n++)
+dir2.push(dir3[n]);
+return dir2;
+}
+catch(e){
+Dark.showDialog("Error from DCMS Library", e);
+}
+},
+removeFolder : function(path){
+try{
+var folder = new java.io.File(path);
+net.zhuoweizhang.mcpelauncher.Utils.clearDirectory(folder);
+folder.delete();
+}
+catch(e){
+Dark.showDialog("Error from DCMS Library", e);
+}
+},
+unZip : function(path1, path2, tf){
+try{
+if(tf){
+var folder = new java.io.File(path);
+folder.mkdir();
+var pp = path2.toString().split("/");
+path2 = path2+pp[pp.length-1].substring(0, lastIndexOf(".");
+}
+var fis = new java.io.FileInputStream(path1);
+var zis = new java.util.zip.ZipInputStream(fis);
+var entry;
+while((entry = zis.getNextEntry())!=null){
+var fos = new java.io.FileOutputStream(path2+"/"+entry.getName());
+var buf = new java.nio.ByteBuffer.allocate(1024).array();
+var len;
+while((len = zis.read(buf))>0){
+fos.write(buf, 0, len);
+}
+zis.closeEntry();
+fos.close();
+}
+zis.close();
+}
+catch(e){
+Dark.showDialog("Error from DCMS Library", e);
+}
+},
+download : function(path, file, url){
+ctx.runOnUiThread(new java.lang.Runnable({
+run: function(){
+try{
+var uri = new android.net.Uri.parse(url);
+var dm = new android.app.DownloadManager.Request(uri);
+dm.setTitle("파일 다운로드");
+dm.setDescription("파일 다운로드 중...");
+dm.setDestinationInExternalPublicDir(path, file);
+dm.setNotificationVisibility(1);
+ctx.getSystemService(android.content.Context.DOWNLOAD_SERVICE).enqueue(dm);
+}
+catch(e){
+Dark.showDialog("Error from DCMS Library", e);
+}
+}
+}));
+}
+
+
+};
+
---
+++ ModPE Expansion Library.js
@@ -0,0 +1,126 @@
+﻿ModPE Expansion Library
+version : 1.0
+Maker : Dark Tornado
+© 2016 Dark Tornado, All rights reserved.
+--------------------
+/*
+Entity.linkWithParticle(e1, e2);
+ModPE.getBlocklauncherVersion = function(tf);
+ModPE.getMyIp = function();
+ModPE.getUserName = function();
+*/
+
+
+Level.spawnSpiderJoky = function(x, y, z){
+var a = Level.spawnMob(x, y, z, 35);
+var b = Level.spawnMob(x, y, z, 34);
+Entity.rideAnimal(b, a);
+};
+
+
+Level.spawnChickenJoky = function(x, y, z){
+var a = Level.spawnMob(x, y, z, 10);
+var b = Level.spawnMob(x, y, z, 32);
+net.zhuoweizhang.mcpelauncher.ScriptManager.nativeZombieSetBaby(b, true);
+Entity.rideAnimal(b, a);
+};
+
+
+Level.isNight = function(){
+return nightCheck();
+};
+
+
+Player.getHealth = function(){
+return Entity.getHealth(Player.getEntity());
+};
+
+
+Player.removeEnchant = function(){
+var itemName = Player.getItemCustomName(Player.getSelectedSlotId());
+Entity.setCarriedItem(Player.getEntity(), getCarriedItem(), Player.getCarriedItemCount(), Player.getCarriedItemData());
+if(itemName!=null) Player.setItemCustomName(Player.getSelectedSlotId(), itemName);
+};
+
+
+Entity.getNearEntities = function(s, r, tf){
+var px, py, pz;
+var ents = [];
+for each(var e in Entity.getAll()){
+px = Math.abs(Entity.getX(s)-Entity.getX(e));
+py = Math.abs(Entity.getY(s)-Entity.getY(e));
+pz = Math.abs(Entity.getZ(s)-Entity.getZ(e));
+if(px<=r&&py<=r&&pz<=r&&z!=e&&(tf||Entity.getEntityTypeId(e)<64)){
+ents.push(e);
+}
+}
+return ents;
+};
+
+
+Entity.removeNameTag = function(e){
+Entity.setNameTag(e, "");
+};
+
+
+Entity.makeBaby = function(e, tf){
+if([32, 36].indexOf(Entity.getEntityTypeId(e))!=-1) net.zhuoweizhang.mcpelauncher.ScriptManager.nativeZombieSetBaby(e, tf);
+else Dark.showDialog("Error from DCMS Library", "Use can use \"Entity.makeBaby(Long entity, Boolean setBaby);\" only Zombies and Zombie Pigs.");
+};
+
+
+Item.getAllItemIds = function(tf){
+var items = [];
+if(tf){
+for(var n=0;n<4096;n++){
+if(Item.isValidItem(n)) items.push(n);
+}
+else{
+for(var n=256;n<4096;n++){
+if(Item.isValidItem(n)) items.push(n);
+}
+}
+}
+return items;
+};
+
+
+Entity.startBattle = function(e1, e2){
+Entity.setTarget(e1, e2);
+Entity.setTarget(e2, e1);
+};
+
+
+Entity.removeEntities = function(typeId){
+for(var e in Entity.getAll()){
+if(Entity.getEntityTypeId(e)==typeId) Entity.remove(e);
+}
+};
+
+
+ModPE.checkBlocklauncherIsPro = function(){
+return net.zhuoweizhang.mcpelauncher.Utils.isPro();
+};
+
+
+ModPE.setWindow10Ui = function(tf){
+net.zhuoweizhang.mcpelauncher.ScriptManager.nativeModPESetDesktopGui(tf);
+};
+
+
+Server.kickAllPlayers = function(){
+for(var e in Entity.getAll()){
+if(Player.isPlayer(e)) Entity.remove(e);
+}
+};
+
+
+Server.getPlayerByName = function(name){
+for(var e in Entity.getAll()){
+if(Player.getName(e)==name) return e;
+}
+};
+
+
+
+
