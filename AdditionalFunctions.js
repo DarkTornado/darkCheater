0cf13bfd527a8bb139d2992e12aa6deef71e749f
Dark Tornado <dt3141592@naver.com>
2016-09-13 20:26:48 +0900

    Add files via upload

--- AdditionalFunctions.js
+++ AdditionalFunctions.js
@@ -44,3 +44,86 @@ clientMessage(e+", "+e.lineNumber);
 }));
 }
 
+function unreplaceDialog(){
+ctx.runOnUiThread(new java.lang.Runnable({
+run: function(){
+try{
+var dialog = new android.app.AlertDialog.Builder(ctx, holoDark);
+var layout = new android.widget.LinearLayout(ctx);
+layout.setOrientation(1);
+var loc0 = new android.widget.TextView(ctx);
+var loc1 = new android.widget.TextView(ctx);
+var loc2 = new android.widget.EditText(ctx);
+var loc3 = new android.widget.TextView(ctx);
+var loc4 = new android.widget.EditText(ctx);
+var loc5 = new android.widget.TextView(ctx);
+var loc6 = new android.widget.TextView(ctx);
+var loc7 = new android.widget.EditText(ctx);
+var loc8 = new android.widget.TextView(ctx);
+var loc9 = new android.widget.EditText(ctx);
+loc0.setText("이 블록(들)을");
+loc0.setTextColor(white);
+loc1.setText("블록 아이디 : ");
+loc1.setTextSize(18);
+loc1.setTextColor(white);
+loc2.setHint("블록 아이디를 입력하세요...");
+loc2.setTextColor(white);
+loc2.setInputType(number);
+loc3.setText("블록 데미지 : ");
+loc3.setTextSize(18);
+loc3.setTextColor(white);
+loc4.setHint("블록 데미지를 입력하세요...");
+loc4.setTextColor(white);
+loc4.setInputType(number);
+loc5.setText("\n이 블록(들)으로");
+loc5.setTextColor(white);
+loc6.setText("블록 아이디 : ");
+loc6.setTextSize(18);
+loc6.setTextColor(white);
+loc7.setHint("블록 아이디를 입력하세요...");
+loc7.setInputType(number);
+loc7.setTextColor(white);
+loc8.setText("블록 데미지 : ");
+loc8.setTextSize(18);
+loc8.setTextColor(white);
+loc9.setHint("블록 데미지를 입력하세요...");
+loc9.setTextColor(white);
+loc9.setInputType(number);
+layout.addView(loc0);
+layout.addView(loc1);
+layout.addView(loc2);
+layout.addView(loc3);
+layout.addView(loc4);
+layout.addView(loc5);
+layout.addView(loc6);
+layout.addView(loc7);
+layout.addView(loc8);
+layout.addView(loc9);
+Dark.noFullScreen(loc2);
+Dark.noFullScreen(loc4);
+Dark.noFullScreen(loc7);
+Dark.noFullScreen(loc9);
+var scroll = android.widget.ScrollView(ctx);
+scroll.addView(layout);
+dialog.setView(scroll);
+dialog.setTitle("빼고 바꾸기");
+dialog.setNegativeButton("취소", null);
+dialog.setPositiveButton("확인", new android.content.DialogInterface.OnClickListener({
+onClick: function(v){
+if(loc4.getText()==""){
+procCmd("unreplace "+loc2.getText()+" "+loc7.getText());
+}
+else{
+procCmd("unchange "+loc2.getText()+" "+loc4.getText()+" "+loc7.getText()+" "+loc9.getText());
+}
+}
+}));
+dialog.show();
+}
+catch(e){
+clientMessaeg(e+", "+e.lineNumber);
+}
+}
+}));
+}
+
---
+++ WorldEditWindow.js
@@ -0,0 +1,61 @@
+﻿try{
+wWin = new DarkFloatingWindow();
+wWin.setTitle("월드 에딧");
+wWin.addText("철칼&철괭이", function(){
+addItemInventory(267, 1, 0);
+addItemInventory(292, 1, 0);
+Dark.toast("지급되었습니다.");
+});
+wWin.addToggleText("머리 블록", "w.h = true;", "w.h = false;", w.h);
+wWin.addToggleText("발 블록", "w.f = true;", "w.f = false;", w.f);
+wWin.addToggleText("블록 교체", "w.c = true;", "w.c = false;", w.c);
+wWin.addToggleText("터치한 블록 정보", "blockInfo = true;", "blockInfo = false;", blockInfo);
+wWin.addText("채우기", "inputBlockId(3);");
+wWin.addText("바꾸기", "replaceDialog(true);");
+wWin.addText("빼고 바꾸기", "unreplaceDialog();");
+wWin.addText("복사", function({
+new java.lang.Thread({
+run : function(){
+procCmd("copy");
+}
+}).start();
+});
+wWin.addText("붙여넣기", function({
+new java.lang.Thread({
+run : function(){
+procCmd("paste");
+}
+}).start();
+});
+wWin.addText("붙여넣기 취소", function({
+if(w.back){
+new java.lang.Thread({
+run : function(){
+procCmd("unpaste");
+}
+}).start();
+}
+else{
+Dark.toast("백업된 지형이 없습니다.");
+}
+});
+wWin.addText(" 원 ", "inputBlockId(13);");
+wWin.addText("찬 원", "inputBlockId(14);");
+wWin.addText("원기둥", "inputBlockId(15);");
+wWin.addText(" 구 ", "inputBlockId(16);");
+wWin.addText("반구", "inputBlockId(17);");
+wWin.addText("찬 구", "inputBlockId(18);");
+wWin.addText(" 벽 ", "inputBlockId(11);");
+wWin.addText("벽(지형유지)", "inputBlockId(12);");
+wWin.addText("직선", "inputBlockId(19);");
+wWin.addText("직선(y포함)", "inputBlockId(20);");
+wWin.addText("기둥 설정", "pillarDialog();");
+wWin.addText("눈내리기", "procCmd('snow');");
+wWin.addText("얼리기", "procCmd('freeze');");
+wWin.addText("나무 삭제", "procCmd('no tree');");
+wWin.addText("물 삭제", "procCmd('no water');");
+wWin.show();
+}
+catch(e){
+Dark.showError(e);
+}
